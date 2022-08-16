749a8fd96c446d659273ae48d4ce686799f7ff51
孟远 <mengyuanzz@126.com>
2022-08-16 16:16:15 +0800

    -循环关键帧、关键帧触发器使用补充；

--- app/src/main/kotlin/com/sws/study/transition/activity/MotionLayoutUse3Activity.kt
+++ app/src/main/kotlin/com/sws/study/transition/activity/MotionLayoutUse3Activity.kt
@@ -1,8 +1,12 @@
 package com.sws.study.transition.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 import com.sws.study.R
+import com.sws.study.utils.toastShow
 
 /**
  * @author mengyuan
@@ -11,16 +15,26 @@ import com.sws.study.R
  * -----------
  * MotionLayout使用-3
  */
-class MotionLayoutUse3Activity : ComponentActivity(){
+class MotionLayoutUse3Activity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion_layout_3)
 
+        findViewById<MotionLayout>(R.id.root).addTransitionListener(object : TransitionAdapter() {
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                toastShow("动画进度{$progress}")
+            }
+        })
 
     }
-
 
 
 }
